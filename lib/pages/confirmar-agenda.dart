febc250b778730d6238623d11b2655fd2c710127
Marcelo Junior <marcelopugasjunior@gmail.com>
2019-08-26 23:49:34 -0300

    Add Page ConfirmarAgendamento

--- lib/pages/confirmar-agenda.dart
+++ lib/pages/confirmar-agenda.dart
@@ -5,30 +5,96 @@ class ConfirmarAgendamento extends StatelessWidget {
   PageController pageController;
   ConfirmarAgendamento(this.pageController);
 
+  _btnConfirmAgenda(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(
+        bottom: 30,
+      ),
+      alignment: Alignment.center,
+      child: RaisedButton(
+          padding: EdgeInsets.symmetric(vertical: 15, horizontal: 25),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(8)),
+          ),
+          color: Color.fromRGBO(194, 155, 79, 1),
+          onPressed: () {},
+          child: Text(
+            "CONFIRMAR AGENDAMENTO",
+            style: TextStyle(color: Colors.white, fontSize: 16),
+          )),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
-        onWillPop: () async {
-          pageController.jumpToPage(0);
-          return false;
-        },
-        child: Scaffold(
-//      appBar: AppBar(
-//        title: Image.asset(
-//          'images/logo.png',
-//          scale: 30,
-//        ),
-//        centerTitle: true,
-//      ),
-//      //drawer: BarberDrawer(),
-          body: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Center(
-                child: Text("Confirmar agendamento"),
+      onWillPop: () async {
+        pageController.jumpToPage(0);
+        return false;
+      },
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(top: 30, bottom: 60),
+            child: Text(
+              "CONFIRME SEU AGENDAMENTO",
+              style: TextStyle(
+                color: Color.fromRGBO(194, 155, 79, 1),
+                fontSize: 18,
+                fontWeight: FontWeight.w700,
               ),
-            ],
+            ),
           ),
-        ));
+          Expanded(
+            child: Container(
+              child: SingleChildScrollView(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.only(
+                        left: (MediaQuery.of(context).size.width * 0.1),
+                      ),
+                      alignment: Alignment.topRight,
+                      child: Text(
+                        "Profissional\n"
+                        "Serviço\n"
+                        "Data\n"
+                        "Horário\n"
+                        "Valor",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
+                          height: 1.5,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(
+                          right: (MediaQuery.of(context).size.width * 0.1)),
+                      child: Text(
+                        "valueeeee\n"
+                        "valueee\n"
+                        "valueeeeeeeee\n"
+                        "valuee\n"
+                        "valueeeeeee",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
+                          height: 1.5,
+                          color: Colors.grey[700],
+                        ),
+                        textAlign: TextAlign.right,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          _btnConfirmAgenda(context)
+        ],
+      ),
+    );
   }
 }
